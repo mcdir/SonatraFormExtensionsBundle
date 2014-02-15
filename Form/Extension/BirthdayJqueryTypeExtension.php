566eb27173c55cdcedf0da7032150662a53a26a5
François Pluchino <francois.pluchino@sonatra.com>
2014-02-15 03:28:09 +0100

    Adds form extension of birthday datetime picker

---
+++ Form/Extension/BirthdayJqueryTypeExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\FormExtensionsBundle\Form\Extension;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BirthdayJqueryTypeExtension extends DateJqueryTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'birthday';
+    }
+}
