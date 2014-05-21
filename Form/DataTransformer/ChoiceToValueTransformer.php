6b0798aa35c7576f5b193c6d2d098e8f755f95d4
François Pluchino <francois.pluchino@sonatra.com>
2014-05-21 16:23:17 +0200

    Changes start block of license phpdoc

--- Form/DataTransformer/ChoiceToValueTransformer.php
+++ Form/DataTransformer/ChoiceToValueTransformer.php
@@ -1,13 +1,13 @@
 <?php
 
-/*
+/**
  * This file is part of the Sonatra package.
-*
-* (c) François Pluchino <francois.pluchino@sonatra.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Sonatra\Bundle\FormExtensionsBundle\Form\DataTransformer;
 
