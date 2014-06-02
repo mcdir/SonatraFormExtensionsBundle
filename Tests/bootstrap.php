b289e8db579f2427901e905b320b3fa2773c4d67
François Pluchino <francois.pluchino@sonatra.com>
2014-06-02 19:55:19 +0200

    Uses custom bootstrap

---
+++ Tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
