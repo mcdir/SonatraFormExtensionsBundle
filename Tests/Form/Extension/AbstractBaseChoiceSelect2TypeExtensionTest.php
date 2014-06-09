99e0ba784ac4e66acc4f9f53bdaf0bac5aee9979
François Pluchino <francois.pluchino@sonatra.com>
2014-06-09 19:08:36 +0200

    Adds base choice select2 form extension tests

---
+++ Tests/Form/Extension/AbstractBaseChoiceSelect2TypeExtensionTest.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\FormExtensionsBundle\Tests\Form\Extension;
+
+use Sonatra\Bundle\FormExtensionsBundle\Form\Extension\BaseChoiceSelect2TypeExtension;
+use Sonatra\Bundle\FormExtensionsBundle\Form\Extension\ChoiceSelect2TypeExtension;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Abstract tests case for base choice select2 form extension type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+abstract class AbstractBaseChoiceSelect2TypeExtensionTest extends TypeTestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        $this->container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $this->request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $this->router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+
+        $this->container->expects($this->any())
+            ->method('get')
+            ->will($this->returnCallback(function ($param) {
+                switch ($param) {
+                    case 'event_dispatcher':
+                        return $this->dispatcher;
+                    case 'request':
+                        return $this->request;
+                    case 'router':
+                        return $this->router;
+                    default:
+                        return null;
+                }
+            }))
+        ;
+
+        /* @var ContainerInterface $container */
+        $container = $this->container;
+
+        $this->factory = Forms::createFormFactoryBuilder()
+            ->addExtensions($this->getExtensions())
+            ->addTypeExtension(new ChoiceSelect2TypeExtension($container, $this->getExtensionTypeName(), 10))
+            ->addTypeExtension(new BaseChoiceSelect2TypeExtension($this->getExtensionTypeName()))
+            ->getFormFactory();
+
+        $this->dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        $this->builder = new FormBuilder(null, null, $this->dispatcher, $this->factory);
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->container = null;
+        $this->request = null;
+        $this->router = null;
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getExtensionTypeName();
+
+    public function test()
+    {
+        //TODO
+        $form = $this->factory->create($this->getExtensionTypeName());
+        $config = $form->getConfig();
+
+        $this->assertTrue($config->hasOption('select2'));
+        $this->assertTrue(array_key_exists('ajax_route', $config->getOption('select2')));
+        $this->assertNull($config->getOption('select2')['ajax_route']);
+        $this->assertEquals('sonatra_form_extensions_ajax_'.$this->getExtensionTypeName(), $config->getAttribute('select2_ajax_route'));
+    }
+}
---
+++ Tests/Form/Extension/TimezoneBaseChoiceSelect2TypeExtensionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\FormExtensionsBundle\Tests\Form\Extension;
+
+/**
+ * Tests case for timezone of base choice select2 form extension type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class TimezoneBaseChoiceSelect2TypeExtensionTest extends AbstractBaseChoiceSelect2TypeExtensionTest
+{
+    protected function getExtensionTypeName()
+    {
+        return 'timezone';
+    }
+}
