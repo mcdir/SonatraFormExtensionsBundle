f8a783eb15d4b5a572a36e8f1ef26557a813b16f
François Pluchino <francois.pluchino@sonatra.com>
2014-06-09 17:17:30 +0200

    Adds currency form type tests

---
+++ Tests/Form/Type/CurrencyTypeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\FormExtensionsBundle\Tests\Form\Type;
+
+use Sonatra\Bundle\FormExtensionsBundle\Form\Type\CurrencyType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\SimpleChoiceList;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Intl\Intl;
+
+/**
+ * Tests case for currency type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class CurrencyTypeTest extends TypeTestCase
+{
+    public function testValidChoiceList()
+    {
+        $type = new CurrencyType();
+        $form = $this->factory->create($type);
+        /* @var SimpleChoiceList $list */
+        $list = $form->getConfig()->getOption('choice_list');
+
+        $this->assertInstanceOf('Symfony\Component\Form\Extension\Core\ChoiceList\SimpleChoiceList', $list);
+
+        $validList = array_keys(Intl::getCurrencyBundle()->getCurrencyNames('en'));
+        sort($validList);
+
+        $choices = $list->getChoices();
+        sort($choices);
+
+        $this->assertSame($validList, $choices);
+    }
+}
